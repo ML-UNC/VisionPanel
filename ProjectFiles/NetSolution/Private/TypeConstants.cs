b2bc6469c433b3bb522a773ae11649c4d8e0775b
ML-UNC <mlucyshyn@uncdairy.com>
2025-12-03 09:20:19 -0500

    Add files via upload

---
+++ ProjectFiles/NetSolution/Private/TypeConstants.cs
@@ -0,0 +1,38 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+namespace VisionTake3
+{
+    public static class ObjectTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("VisionTake3");
+        public static readonly NodeId TBD1 = new NodeId(namespaceIndex, new Guid("c2760f706c372a05c7a1b1bb958377e4"));
+        public static readonly NodeId MainWindow = new NodeId(namespaceIndex, new Guid("01a7e8933afd7c95fe894b0859308138"));
+        public static readonly NodeId Maintenance = new NodeId(namespaceIndex, new Guid("f27e34860c872b8fcdb1e3cf79246154"));
+        public static readonly NodeId Alarming = new NodeId(namespaceIndex, new Guid("28aea319facb1888622969bc171c983a"));
+        public static readonly NodeId ProductPicker = new NodeId(namespaceIndex, new Guid("4ffad92028278cc88f34597adef87bbc"));
+        public static readonly NodeId CameraBrowser = new NodeId(namespaceIndex, new Guid("2299cf96dade5a891a8537b4b382bc00"));
+        public static readonly NodeId TBD2 = new NodeId(namespaceIndex, new Guid("4c0e8a57c7877b34698186f5eb5cbef9"));
+        public static readonly NodeId Lane1ProductSelect = new NodeId(namespaceIndex, new Guid("56fd7ddf98af7891ad61f1b787acb8f7"));
+        public static readonly NodeId DVDPlayer = new NodeId(namespaceIndex, new Guid("cc08f30939a47855238cf9a88eaeabae"));
+        public static readonly NodeId Lane2ProductSelect = new NodeId(namespaceIndex, new Guid("b69ec2feacea7f7b9e8dc5efe588919c"));
+        public static readonly NodeId NavTile = new NodeId(namespaceIndex, new Guid("15645b05ab6832cc2bcb69a3186d0108"));
+        public static readonly NodeId PopUpWindow = new NodeId(namespaceIndex, new Guid("622f8b38939f608023a76655bb0e7ede"));
+        public static readonly NodeId PopUp1 = new NodeId(namespaceIndex, new Guid("c0cce9a98430129464ce42a5528dc056"));
+        public static readonly NodeId NavPos = new NodeId(namespaceIndex, new Guid("d832ea3f3b284274e2db1b2363436ce2"));
+        public static readonly NodeId navigationTile = new NodeId(namespaceIndex, new Guid("576f2b83c381e838195cd692accb477e"));
+        public static readonly NodeId Menu = new NodeId(namespaceIndex, new Guid("514b18258df22a45490d6846bf57381b"));
+        public static readonly NodeId PopUp = new NodeId(namespaceIndex, new Guid("e5d6aaf94524018983651fb7ed8d8a69"));
+        public static readonly NodeId Navigation_Pos = new NodeId(namespaceIndex, new Guid("0546cbc235a13743576da02133a32566"));
+        public static readonly NodeId PopUp_Obj = new NodeId(namespaceIndex, new Guid("ffc4dcda380d76cc4d4ce54d4365b1b0"));
+    }
+
+    public static class VariableTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("VisionTake3");
+    }
+}
